073b10e9f0a5c34e731ced9dd940710ff44ad139
marchesido <marchesido@gmail.com>
2025-07-12 12:40:53 -0300

    footer components

---
+++ components/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer" data-bs-theme="dark">
+    <p class="text-center">
+        Desenvolvido por Douglas A. Marchesi
+    </p>
+    <h1 class="text-center">
+        <a class="navbar-brand" href="erro.php">
+            <img src="imagens/footer.png" class="img-fluid" alt="GlamourTech">
+        </a>
+    </h1>
+</footer>
