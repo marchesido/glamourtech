ea3b92dbfa1b668439732e72c6e4a8206c30eb25
marchesido <marchesido@gmail.com>
2025-07-12 22:18:00 -0300

    page erro

---
+++ erro.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <p class="text-center">
+        <img src="../imagens/404.png" class="img-fluid" alt="Erro 404">
+    </p>
+    <h2 class="text-center">
+        A página que está tentando acessar não existe ou foi removida!
+    </h2>
+    <p class="text-center">
+        <a href="home" title="Página Inicial" class="btn btn-primary">
+            Voltar à página inicial
+        </a>
+    </p>
+</div>
