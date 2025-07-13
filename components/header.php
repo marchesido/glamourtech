5e0b9477349ccbb139859f227a67cd8e2623ce3c
marchesido <marchesido@gmail.com>
2025-07-12 22:20:37 -0300

    atualizacao component header

--- components/header.php
+++ components/header.php
@@ -1,13 +1,15 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">
-            <img src="imagens/logotransparente.png" alt="Logo">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+        <a class="navbar-brand d-flex align-items-center" href="index.php">
+            <img src="imagens/logotransparente.png" alt="Logo" class="logo-img">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
@@ -20,7 +22,11 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'contato.php' ? 'active' : ''; ?>"
-                        href="paginas/contato.php">Contato</a>
+                        href="contato.php">Contato</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'sobre.php' ? 'active' : ''; ?>"
+                        href="sobre.php">Sobre</a>
                 </li>
             </ul>
         </div>
